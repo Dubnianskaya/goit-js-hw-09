1ff3a92ac36dcca29a79f1f5d281100595576e72
Maryna Dubnianskaya <dubnianskaya.marina@gmail.com>
2021-11-02 23:58:07 +0300

    finished first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+import randomColor from './helpers/01-random-color';
 
+const refs = {
+    startBtn: document.querySelector("button[data-start]"),
+    stopBtn: document.querySelector("button[data-stop]"),
+    body: document.querySelector("body"),
+};
+refs.startBtn.addEventListener("click", onColorChangeStart);
+refs.stopBtn.addEventListener("click", onColorChangeStop);
+let colorId = null;
+
+function onColorChangeStart() {
+    colorId = setInterval(() => {
+        refs.body.style.backgroundColor = randomColor();
+    }, 1000)
+    refs.startBtn.setAttribute('disabled', true)
+}
+function onColorChangeStop() {
+    clearInterval(colorId);
+    refs.startBtn.removeAttribute('disabled')
+}
