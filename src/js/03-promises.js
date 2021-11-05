8b1be8814b4941525e9b9eca15f1c3082238ae57
Maryna Dubnianskaya <dubnianskaya.marina@gmail.com>
2021-11-05 16:55:59 +0200

    finished third task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,44 +1,38 @@
+import Notiflix from 'notiflix';
 
-const refs = {
-  form: document.querySelector(".form"),
-  submitBtn: document.querySelector("button"),
-}
+const form = document.querySelector(".form");
+form.addEventListener("submit", onFormData);
 
-refs.form.addEventListener("submit", onFormData);
+function createPromise(position, delay) {
+    const shouldResolve = Math.random() > 0.3;
+    return new Promise((resolve, reject) => {
+      setTimeout(() => { 
+        if (shouldResolve) {
+          resolve({position, delay});
+        } else {
+          reject({position, delay});
+        }
+      }, delay)
+  })
+  }
 
 function onFormData(event) {
   event.preventDefault()
   const {
     elements: { delay, step, amount}
   } = event.currentTarget;
-  const firstDelay = delay.value;
-  let totalDelay = firstDelay;
-  let numberOfPosition = 0;
-  for (let i = 0; i < amount.value; i += 1) {
-    totalDelay += step.value;
-    numberOfPosition += 1;
-    createPromise(numberOfPosition, totalDelay);
-    createPromise().then(value => {
-    console.log(value);
-  })
-  .catch(error => {
-    console.log(error);
-  });
+  let firstDelay = Number(delay.value);
+  let delayStep = Number(step.value);
+  let currentAmount = Number(amount.value);
+  for (let i = 1; i <= currentAmount; i += 1) {
+      createPromise(i, firstDelay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      })
+      firstDelay += delayStep;
   }
-}
-
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-  //   resolve(i), firstDelay);
-  // }).then(value => console.log(value));
-  if (shouldResolve) {
-    resolve(`✅ Fulfilled promise ${position} in ${delay}ms`)
-  } else {
-    reject(`❌ Rejected promise ${position} in ${delay}ms`)
-  }
-}, delay);
-  });
 }
 
